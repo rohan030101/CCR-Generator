4f3eb2968456181ed9c68ed34817028bd4ab0cc4
Rohan Jeevan Janrao <52947469+rohan030101@users.noreply.github.com>
2022-12-08 08:35:41 +0000

    javascript file edited

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("CCR Genrator");
+
+function generate_ccr(){
+    // console.log('Working');
+var ccr_no = document.getElementById('ccr_no').value;
+var date = document.getElementById('date').value;
+var emp_name = document.getElementById('employee_name').value;
+var dept = document.getElementById('department').value;
+var cmp_serial= document.getElementById('cmp_serial').value;
+var eqp_details= document.getElementById('eqp_details').value;
+var call_status= document.getElementById('call_status').value;
+var remark = document.getElementById('remark').value;
+return ccr_no,date,emp_name,dept,cmp_serial,eqp_details,
+}
