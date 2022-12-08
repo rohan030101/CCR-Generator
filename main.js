6f7f6acb9f7b14ea8e7bdcc479f389f8d86eb299
Rohan030101 <janraorohan2@gmail.com>
2022-12-08 21:55:47 +0530

    Finalized all the variables and getting methods.

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-
-function generate_ccr(){
-    // console.log('Working');
-var ccr_no = document.getElementById('ccr_no').value;
-var date = document.getElementById('date').value;
-var emp_name = document.getElementById('employee_name').value;
-var dept = document.getElementById('department').value;
-var cmp_serial= document.getElementById('cmp_serial').value;
-var eqp_details= document.getElementById('eqp_details').value;
-var call_status= document.getElementById('call_status').value;
-var remark = document.getElementById('remark').value;
-return ccr_no,date,emp_name,dept,cmp_serial,eqp_details,
+function generate_ccr() {
+  var ccr_no = document.getElementById("ccr_no").value;
+  var date = document.getElementById("date").value;
+  var emp_name = document.getElementById("employee_name").value;
+  var dept = document.getElementById("department").value;
+  var cmp_serial = document.getElementById("cmp_serial").value;
+  var eqp_details = document.getElementById("eqp_details").value;
+  var call_status = document.getElementById("call_status").value;
+  var remark = document.getElementById("remark").value;
+  var change = document.getElementById("preview");
+  //   change.innerHTML =
+  //     ccr_no +
+  //     date +
+  //     emp_name +
+  //     dept +
+  //     cmp_serial +
+  //     eqp_details +
+  //     call_status +
+  //     remark;
+  // console.log(call_status);
+  // console.log(
+  //   ccr_no,
+  //   +"\n" + date,
+  //   +"\n" + emp_name,
+  //   +"\n" + dept,
+  //   +"\n" + cmp_serial,
+  //   +"\n" + eqp_details,
+  //   +"\n" + call_status,
+  //   +"\n" + remark;
 }
+// console.log(generate_ccr());
