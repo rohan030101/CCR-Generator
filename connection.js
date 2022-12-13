788619fe937b2afe483b21aa2f7451e8a96ae3ea
Rohan030101 <janraorohan2@gmail.com>
2022-12-13 22:27:58 +0530

    Made some change is connection.js

--- connection.js
+++ connection.js
@@ -12,6 +12,8 @@
 //   var conn = new sql.ConnectionPool(dbconfig);
 //   var req = new sql.Request(conn);
 
+// const { exit } = require("process");
+
 //   conn.connect(function (err) {
 //     if (err) {
 //       console.log(err);
@@ -29,26 +31,37 @@
 // }
 
 // getemp();
-function connectiontoDatabase() {
-  var sql = require("mssql");
-  var dbconfig = {
-    server: "localhost\\SQLEXPRESS",
-    database: "testing",
-    user: "Rohan",
-    password: "Database@030101",
-    port: 1433,
-    trustServerCertificate: true,
-  };
-  var conn = new sql.ConnectionPool(dbconfig);
-  var req = new sql.Request(conn);
+var sql = require("mssql");
+var checkDatabase =
+  "IF EXISTS(SELECT * FROM master.sys.sysdatabases WHERE name='CCR Database') BEGIN PRINT 'Database Exists' END";
+// var display = "SELECT * FROM [CCR_Database].[dbo].[CCR_Datavar
 
-  conn.connect(function (err) {
-    if (err) {
+var display =
+  "SELECT TOP (1000) [CCR_NO],[date],[Emp_name],[Dept],[Cmp_Serial],[Eqp_detls],[Call_Status],[Remark] FROM [CCR Database].[dbo].[CCR_Data]";
+var push =
+  "INSERT into [CCR_Database].[dbo].[CCR_Data] (CCR_No,Emp_name,Dept,Cmp_Serial,Eqp_detls,Call_Status,Remark) VALUES (1,'ABC','IT',1234,'BNM','QWER','TYUI');";
+var dbconfig = {
+  server: "localhost\\SQLEXPRESS",
+  database: "testing",
+  user: "Rohan",
+  password: "Database@030101",
+  port: 1433,
+  trustServerCertificate: true,
+};
+var conn = new sql.ConnectionPool(dbconfig);
+var req = new sql.Request(conn);
+
+conn.connect(function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("Connection Sucess.");
+  conn.query(
+    "INSERT into [CCR Database].[dbo].[CCR_Data] (CCR_No,Emp_name,Dept,Cmp_Serial,Eqp_detls,Call_Status,Remark) VALUES (1,'ABC','IT',1234,'BNM','QWER','TYUI');",
+    (err, result) => {
+      console.log(result);
       console.log(err);
-      return;
     }
-    return console.log("Connection Sucess.");
-    conn.close();
-  });
-}
-connectiontoDatabase();
+  );
+});
