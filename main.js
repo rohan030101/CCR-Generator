ed1fc208047e8f9700cdc1aff3db2d7d6e1b8e41
Rohan030101 <janraorohan2@gmail.com>
2022-12-21 22:28:17 +0530

    complete

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+window.onload = function () {
+  get_Date();
+  console.log("onload");
+};
 function generate_ccr() {
   var ccr_no = document.getElementById("ccr_no").value;
-  var date = document.getElementById("date").value;
+  var date = document.getElementById("dt").value;
   var emp_name = document.getElementById("employee_name").value;
   var dept = document.getElementById("department").value;
   var cmp_serial = document.getElementById("cmp_serial").value;
@@ -18,14 +22,40 @@ function generate_ccr() {
   //     call_status +
   //     remark;
   // console.log(call_status);
-  // console.log(
-  //   ccr_no,
-  //   +"\n" + date,
-  //   +"\n" + emp_name,
-  //   +"\n" + dept,
-  //   +"\n" + cmp_serial,
-  //   +"\n" + eqp_details,
-  //   +"\n" + call_status,
-  //   +"\n" + remark;
+  console.log(ccr_no);
+  console.log(date);
+  console.log(emp_name);
+  console.log(dept);
+  console.log(cmp_serial);
+  console.log(eqp_details);
+  console.log(call_status);
+  console.log(remark);
+  // console.log(change);
 }
 // console.log(generate_ccr());
+
+function getDate() {
+  var today = new Date();
+  var dd = today.getDate();
+  // console.log(dd);
+  var mm = today.getMonth() + 1; //January is 0!
+  // console.log(mm);
+  var yyyy = today.getFullYear();
+  // console.log(yyyy);
+
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+
+  today = dd + "-" + mm + "-" + yyyy;
+  // console.log(today);
+  document.getElementById("dt").value = today;
+}
+
+window.onload = function () {
+  getDate();
+};
