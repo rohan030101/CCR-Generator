c5e2173a1b9e3c9d3b2de179c2062df028d0fd02
Rohan030101 <janraorohan2@gmail.com>
2022-12-23 20:13:33 +0530

    Make some changes in main.css fiell

--- connection.js
+++ connection.js
@@ -1,67 +1,21 @@
-// var sql = require("mssql");
-//     var dbconfig = {
-//       server: "localhost\\SQLEXPRESS",
-//       database: "testing",
-//       user: "Rohan",
-//       password: "Database@030101",
-//       port: 1433,
-//       trustServerCertificate: true,
-//     };
-// }
-// function getemp() {
-//   var conn = new sql.ConnectionPool(dbconfig);
-//   var req = new sql.Request(conn);
+// const { doesNotMatch } = require("assert");
+const { MongoClient } = require("mongodb");
 
-// const { exit } = require("process");
+//connection url
+const url = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(url);
 
-//   conn.connect(function (err) {
-//     if (err) {
-//       console.log(err);
-//       return;
-//     }
-//     req.query("SELECT * from tblPatient", function (err, recordset) {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log(recordset);
-//       }
-//       conn.close();
-//     });
-//   });
-// }
+//databse name
+const dbName = "Inventory";
 
-// getemp();
-var sql = require("mssql");
-var checkDatabase =
-  "IF EXISTS(SELECT * FROM master.sys.sysdatabases WHERE name='CCR Database') BEGIN PRINT 'Database Exists' END";
-// var display = "SELECT * FROM [CCR_Database].[dbo].[CCR_Datavar
+//function to connect to database.
+async function main() {
+  await client.connect();
+  console.log("Connected Sucessfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("CCR");
+  return "done";
+}
 
-var display =
-  "SELECT TOP (1000) [CCR_NO],[date],[Emp_name],[Dept],[Cmp_Serial],[Eqp_detls],[Call_Status],[Remark] FROM [CCR Database].[dbo].[CCR_Data]";
-var push =
-  "INSERT into [CCR_Database].[dbo].[CCR_Data] (CCR_No,Emp_name,Dept,Cmp_Serial,Eqp_detls,Call_Status,Remark) VALUES (1,'ABC','IT',1234,'BNM','QWER','TYUI');";
-var dbconfig = {
-  server: "localhost\\SQLEXPRESS",
-  database: "testing",
-  user: "Rohan",
-  password: "Database@030101",
-  port: 1433,
-  trustServerCertificate: true,
-};
-var conn = new sql.ConnectionPool(dbconfig);
-var req = new sql.Request(conn);
-
-conn.connect(function (err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log("Connection Sucess.");
-  conn.query(
-    "INSERT into [CCR Database].[dbo].[CCR_Data] (CCR_No,Emp_name,Dept,Cmp_Serial,Eqp_detls,Call_Status,Remark) VALUES (1,'ABC','IT',1234,'BNM','QWER','TYUI');",
-    (err, result) => {
-      console.log(result);
-      console.log(err);
-    }
-  );
-});
+main().then(console.log).catch(console.error);
+// .finally(() => client.close());
