768480d130115e94b63fdfc8a219b8e9d65009a2
Rohan030101 <janraorohan2@gmail.com>
2022-12-27 22:10:27 +0530

    Added pie char using chart.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,42 @@
-function generate_ccr() {
-  var ccr_no = document.getElementById("ccr_no").value;
-  var date = document.getElementById("dt").value;
-  var emp_name = document.getElementById("employee_name").value;
-  var dept = document.getElementById("department").value;
-  var cmp_serial = document.getElementById("cmp_serial").value;
-  var eqp_details = document.getElementById("eqp_details").value;
-  var call_status = document.getElementById("call_status").value;
-  var remark = document.getElementById("remark").value;
-  var change = document.getElementById("preview");
+// import Chart from "chart.js";
+console.log("This is working ");
 
-  console.log(ccr_no);
-  console.log(date);
-  console.log(emp_name);
-  console.log(dept);
-  console.log(cmp_serial);
-  console.log(eqp_details);
-  console.log(call_status);
-  console.log(remark);
-}
+// var part = ["Mouse", "Keyboard", "Monitor", "MohtheBoard"];
+// var values = [10, 20, 5, 5];
+// var pieColors = ["#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145"];
 
-function getDate() {
-  var today = new Date();
-  var dd = today.getDate();
-  // console.log(dd);
-  var mm = today.getMonth() + 1; //January is 0!
-  // console.log(mm);
-  var yyyy = today.getFullYear();
-  // console.log(yyyy);
+// new Chart("myChart", {
+//   type: "pie",
+//   data: {
+//     labels: part,
+//     datasets: [{ backgroundColor: pieColors, data: values }],
+//   },
+//   options: {
+//     title: {
+//       display: true,
+//       text: "Inventory",
+//     },
+//   },
+// });
+var xValues = ["Monitor", "Keyboard", "Mouse", "Cabinet", "Motheboard"];
+var yValues = [10, 8, 8, 2, 2];
+var barColors = ["#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145"];
 
-  if (dd < 10) {
-    dd = "0" + dd;
-  }
-
-  if (mm < 10) {
-    mm = "0" + mm;
-  }
-
-  today = dd + "-" + mm + "-" + yyyy;
-  // console.log(today);
-  document.getElementById("dt").value = today;
-}
-
-// window.onload = function () {
-//   getDate();
-// };
-
-function addDiv() {
-  const div = document.createElement("div");
-  // div.className("databox");
-  div.innerHTML = "This is a div";
-  // div.id("dataBox");
-
-  const add = document.getElementById("upper");
-  add.appendChild(div);
-}
+new Chart("myChart", {
+  type: "pie",
+  data: {
+    labels: xValues,
+    datasets: [
+      {
+        backgroundColor: barColors,
+        data: yValues,
+      },
+    ],
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Inventory",
+    },
+  },
+});
