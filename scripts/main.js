5e217d62fb10e7ee3773247b16658b498d8c33c6
Rohan Jeevan Janrao <52947469+rohan030101@users.noreply.github.com>
2022-12-28 09:49:47 +0000

    Droped the pie chart will use number indication for display

--- scripts/main.js
+++ scripts/main.js
@@ -18,25 +18,25 @@ console.log("This is working ");
 //     },
 //   },
 // });
-var xValues = ["Monitor", "Keyboard", "Mouse", "Cabinet", "Motheboard"];
-var yValues = [10, 8, 8, 2, 2];
-var barColors = ["#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145"];
+// var xValues = ["Monitor", "Keyboard", "Mouse", "Cabinet", "Motheboard"];
+// var yValues = [10, 8, 8, 2, 2];
+// var barColors = ["#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145"];
 
-new Chart("myChart", {
-  type: "pie",
-  data: {
-    labels: xValues,
-    datasets: [
-      {
-        backgroundColor: barColors,
-        data: yValues,
-      },
-    ],
-  },
-  options: {
-    title: {
-      display: true,
-      text: "Inventory",
-    },
-  },
-});
+// new Chart("myChart", {
+//   type: "pie",
+//   data: {
+//     labels: xValues,
+//     datasets: [
+//       {
+//         backgroundColor: barColors,
+//         data: yValues,
+//       },
+//     ],
+//   },
+//   options: {
+//     title: {
+//       display: true,
+//       text: "Inventory",
+//     },
+//   },
+// });
